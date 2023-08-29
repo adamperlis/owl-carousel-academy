3a12fd2e352679eab668def3076ac89479b0d29f
Adam Perlis <adam.perlis@academyux.com>
2023-08-29 08:59:12 -0400

    Update owl-carousel-academy.js

--- owl-carousel-academy.js
+++ owl-carousel-academy.js
@@ -1,4 +1,3 @@
-<!-- owlcaroule slider -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
